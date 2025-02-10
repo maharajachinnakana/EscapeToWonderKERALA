a5c5c1a5bf2c47b979da99e00569a754c8d7269b
maharajachinnakana <maharajachinnakanal@gmail.com>
2025-02-10 09:48:50 +0530

    update

--- Places to Visit /webpage.js
+++ Places to Visit /webpage.js
@@ -23,11 +23,59 @@
   //selecting the round
   //selecting the line
   //selecting the menu items 
-  var round=document.querySelector(.round)
-  var line=document.querySelectorAll(.line)
-  var menu=document.querySelector(.menu-items)
-  function menubar() {
-    round.style.width(200px)
-    round.style.height(200px)
-    round.style.
+  var menui = document.querySelector(".round");
+var menuItem = document.querySelector(".menu-items");
+var icon = document.querySelector(".icon");
+var lines= document.querySelectorAll(".line")
+var menuOpen = false;
+function menu() {
+  // Scale the round button
+  if (!menuOpen){
+      menui.style.marginLeft = "90%"
+      menui.style.marginTop = "-22%"
+      menui.style.borderColor = "transparent"
+      menuItem.style.visibility = "visible"
+  // Adjust the icon margin left
+      icon.style.marginLeft = "0";
+      icon.style.marginTop = "0"// Reset icon margin left (optional)
+
+      var fx = lines[0]
+      var sx = lines[1]
+      var tx = lines[2]
+      sx.style.marginTop = "-12px";
+      fx.style.marginTop = "15px";
+      tx.style.visibility = "hidden";
+      sx.style.transform = "rotate(45deg)"
+      fx.style.transform = "rotate(-45deg)"
+      sx.style.backgroundColor = "red"
+      fx.style.backgroundColor = "red"
+      fx.style.transition = "transform 2s"
+      sx.style.transition = "transform 2s"
+      menuOpen = true;
+      
   }
+  else {
+      menui.style.marginLeft = "0%"
+      menui.style.marginTop = "-67px"
+      menui.style.borderColor = "orange"
+      menuItem.style.visibility = "hidden"
+      // Adjust the icon margin left
+      icon.style.marginLeft = "87%"; // Reset icon margin left (optional)
+
+      var fx = lines[0]
+      var sx = lines[1]
+      var tx = lines[2]
+      sx.style.backgroundColor = "orange"
+      fx.style.backgroundColor = "orange"
+      sx.style.transform = "rotate(0deg)"
+      fx.style.transform = "rotate(0deg)"
+      fx.style.transition = "transform 2s"
+      sx.style.transition = "transform 2s"
+      sx.style.marginTop = "0px";
+      fx.style.marginTop = "0px";
+      tx.style.visibility = "visible";
+      tx.style.transition = "opacity 2s"
+      menuOpen = false;
+    
+  }
+}
