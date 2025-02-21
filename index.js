f9e596512acf61ca601236577b735d08c6a9439e
maharajachinnakana <maharajachinnakanal@gmail.com>
2025-02-21 16:36:02 +0530

    update

--- index.js
+++ index.js
@@ -179,23 +179,3 @@ function openForm() {
     
   // Tab to edit
 }
-
-
-//sarakku oppenr 
-/*var opener=document.querySelectorAll('.opener')
-var sarakku=document.querySelectorAll('.sarakku')
-var hiddenKeywords=document.querySelectorAll('.hidden-keyword')
-var closed=true
-function keyword() {
-  // Tab to edit'bloc
-  opener.forEach(function(cap){
-  cap.addEventListener('click'function() {
-      if (sarakku.style.display ==='none'){
-        sarakku.style.display='block'
-      }
-      else{
-        sarakku.style.display='block'
-      }
-    }
-  }
-keyword()*/
